9ac255da71e51b701833c2d382d7f87dd10b8532
xiaoxinwan <lixinxi1010@gmail.com>
2019-01-29 04:16:03 +0800

    完成极速借贷组件

--- app/js/home/index.vue
+++ app/js/home/index.vue
@@ -3,6 +3,8 @@
       <Heador></Heador>
       <Slider></Slider>
       <Novice></Novice>  
+      <Borrow></Borrow> 
+      <Money></Money>
     </div>
 </template>
 
@@ -10,8 +12,10 @@
 import Heador from '../public/header.vue'
 import Slider from './hslider.vue'
 import Novice from './novice.vue'
+import Borrow from './borrow.vue'
+import Money from './money.vue';
 export default {
-  components:{Heador,Slider,Novice}
+  components:{Heador,Slider,Novice,Borrow,Money}
 }
 </script>
 
